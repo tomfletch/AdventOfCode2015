aac6a8f6d77e24436719eef4c5837933f9e451f0
Tom Fletcher <thomasmatthewfletcher@gmail.com>
2021-09-16 07:54:40 +0100

    Complete day 4

---
+++ 04/day_04_1_test.py
@@ -0,0 +1,7 @@
+from day_04_1 import lowest_suffix
+
+def test_1():
+    assert lowest_suffix('abcdef') == 609043
+
+def test_2():
+    assert lowest_suffix('pqrstuv') == 1048970
---
+++ 04/day_04_2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import hashlib
+
+
+def lowest_suffix(key):
+    suffix = 1
+
+    while True:
+        md5 = calculate_md5(key, suffix)
+
+        if starts_with_five_zeros(md5):
+            break
+
+        suffix += 1
+
+    return suffix
+
+
+def calculate_md5(key, suffix):
+    value = key + str(suffix)
+    md5 = hashlib.md5(str(value).encode('utf-8'))
+    return md5.hexdigest()
+
+
+def starts_with_five_zeros(value):
+    return value.startswith('000000')
+
+
+def main():
+    key = 'iwrupvqb'
+    suffix = lowest_suffix(key)
+    print(f'Suffix: {suffix}')
+
+if __name__ == '__main__':
+    main()
