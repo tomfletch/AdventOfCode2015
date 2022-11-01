e214a6f5bd18c87b3feab293ff4d976b448f7c29
Tom Fletcher <thomasmatthewfletcher@gmail.com>
2022-11-01 20:20:58 +0000

    Complete day 24

---
+++ 24/day_24_1.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+from typing import List, Set
+
+def main():
+    weights = read_package_weights()
+    total_weight = sum(weights)
+    weight_per_group = total_weight // 3
+
+    group_options = weights_that_sum_options(weights, weight_per_group)
+    print('Found groups')
+    group_options.sort(key=lambda option: (len(option), multiply_set(option)))
+    print('Sorted groups')
+    grouping_option = next(find_grouping_options(group_options))
+
+    print('QE of first group:', multiply_set(grouping_option[0]))
+
+def multiply_set(s: Set[int]):
+    total = 1
+
+    for item in s:
+        total *= item
+
+    return total
+
+def find_grouping_options(group_options):
+    for group_1 in group_options:
+
+        for group_2 in group_options:
+            if group_2.intersection(group_1):
+                continue
+
+            for group_3 in group_options:
+                if group_3.intersection(group_1) or group_3.intersection(group_2):
+                    continue
+
+                yield [group_1, group_2, group_3]
+
+
+def weights_that_sum_options(weights: Set[int], total: int):
+    options: List[Set[int]] = []
+
+    for weight in weights:
+        if weight < total:
+            remaining_weights = {w for w in weights if w > weight}
+
+            if remaining_weights:
+                remaining_total = total - weight
+                remaining_weights_options = weights_that_sum_options(remaining_weights, remaining_total)
+
+                for remaining_weights in remaining_weights_options:
+                    options.append({weight} | remaining_weights)
+        elif weight == total:
+            options.append({weight})
+
+    return options
+
+
+def read_package_weights():
+    with open('input.txt') as file:
+        return {int(line) for line in file}
+
+if __name__ == '__main__':
+    main()
